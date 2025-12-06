97702f8a739b508d2b0ad04392c9f7c67b6b40ae
kaez336 <kelvin@envazia.com>
2025-12-06 10:03:53 +0700

    Update init_pmail.sql

--- init_pmail.sql
+++ init_pmail.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS user (
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     salt TEXT,
-    is_admin INTEGER DEFAULT 0,
+    is_admin INTEGER NOT NULL DEFAULT 0,
     create_time DATETIME,
     update_time DATETIME
 );
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS email (
     to_user TEXT,
     subject TEXT,
     content TEXT,
-    status INTEGER DEFAULT 0,
+    status INTEGER NOT NULL DEFAULT 0,
     create_time DATETIME,
     update_time DATETIME
 );
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS user_email (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     email_id INTEGER NOT NULL,
-    status INTEGER DEFAULT 0
+    status INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS version (
@@ -58,16 +58,18 @@ CREATE TABLE IF NOT EXISTS version (
     info TEXT
 );
 
+-- Insert admin user
 INSERT INTO user (username, password, salt, is_admin, create_time, update_time)
 VALUES (
     'admin',
-    '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
+    '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', -- password: admin
     '',
     1,
     datetime('now'),
     datetime('now')
 );
 
+-- Insert version info
 INSERT INTO version (info)
 VALUES ('v2.9.9');
 
